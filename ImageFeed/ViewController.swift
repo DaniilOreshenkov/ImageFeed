3bc9bb366a933c9e683de50879949f0847c3e938
juniorTolmach <juniortolmach@gmail.com>
2024-11-22 23:52:19 +0300

    git добавлен в проект

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Daniil Oreshenkov on 22.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
